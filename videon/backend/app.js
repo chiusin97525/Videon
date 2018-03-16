fd4fac95df8c04142138b9df100725d6d7e13530
chiusin97525 <sinchi.chiu97525@gmail.com>
2018-03-15 23:01:30 -0400

    Implemented login and signup function

--- videon/backend/app.js
+++ videon/backend/app.js
@@ -9,17 +9,43 @@ const cookie = require('cookie');
 const session = require('express-session');
 const forceSsl = require('force-ssl-heroku');
 const MongoClient = require('mongodb').MongoClient;
+const validator = require('validator');
 const app = express();
 
 // require custom modules
 const user = require('./user')
 
+// custom messages
+const ERRMSG_BAD_USERNAME = "Bad username input";
+const ERRMSG_BAD_EMAIL = "Not a valid email address";
+
 
 // server settings
+
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+app.use(session({
+    secret: 'blackbird flies',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {httpOnly: true, sameSite: true}
+}));
+
+
+app.use(function(req, res, next){
+    var username = (req.session.username)? req.session.username : '';
+    res.setHeader('Set-Cookie', cookie.serialize('username', username, {
+          path : '/', 
+          maxAge: 60 * 60 * 24 * 7 // 1 week in number of seconds
+    }));
+    next();
+});
+
 // frontend files
 app.use(express.static('frontend'));
 
@@ -47,13 +73,26 @@ var force_https = function(){
 }
 force_https();
 
+//----------------------------------------------------------------------------------
+var checkUsername = function(req, res, next) {
+    if (!validator.isAlphanumeric(req.body.username)) return res.status(400).end(ERRMSG_BAD_USERNAME);
+    next();
+};
+
+var checkEmail = function(req, res, next){
+     if (!validator.isEmail(req.body.email)) return res.status(400).end(ERRMSG_BAD_EMAIL);
+     next();
+}
+//----------------------------------------------------------------------------------
 
 // SIGN IN/OUT/UP
-app.post('/signup/', function (req, res, next) {
+
+// curl -X POST -d "username=admin&password=admin&email=abc@gmail.com" http://192.168.1.107:5000/register/
+app.post('/register/',checkUsername, checkEmail, function (req, res, next) {
     if (!('username' in req.body)) return res.status(400).end('username is missing');
     if (!('password' in req.body)) return res.status(400).end('password is missing');
-    var username = req.body.username;
-    var password = req.body.password;
+    if (!('email' in req.body)) return res.status(400).end('email is missing');
+    var userInfo = {username: req.body.username, password: req.body.password, email: req.body.email};
     MongoClient.connect(uri, function(err, client) {
         if (err){
             console.log(err);
@@ -61,7 +100,27 @@ app.post('/signup/', function (req, res, next) {
         }else{
             console.log("DB connection success");
             const database = client.db(dbName);
-            user.signUp(res, username, password, database, function(){
+            user.register(res, req, userInfo, database, function(){
+                // close the client connection to the database
+                client.close();
+            });
+          }
+    });
+});
+
+// curl -X POST -d "username=admin&password=admin" -c cookie.txt http://192.168.1.107:5000/login/
+app.post('/login/',checkUsername, function (req, res, next) {
+    if (!('username' in req.body)) return res.status(400).end('username is missing');
+    if (!('password' in req.body)) return res.status(400).end('password is missing');
+    var userInfo = {username: req.body.username, password: req.body.password};
+    MongoClient.connect(uri, function(err, client) {
+        if (err){
+            console.log(err);
+            return res.status(500).end(err);
+        }else{
+            console.log("DB connection success");
+            const database = client.db(dbName);
+            user.login(res, req, userInfo, database, function(){
                 // close the client connection to the database
                 client.close();
             });
--- videon/backend/user.js
+++ videon/backend/user.js
@@ -5,35 +5,61 @@ module.exports = (function(){
     const collectionName = "users";
     var user = {};
 
+    const ERRMSG_ACCESS_DENIED = "access denied";
+
+
     function generateHash (password, salt){
         var hash = crypto.createHmac('sha512', salt);
         hash.update(password);
         return hash.digest('base64');;
     }
 
-    user.signUp = function(res, username, password, database, callback){
+    function response(res, statusCode, ending, callback){
+        callback;
+        return res.status(statusCode).end(ending);
+    }
+
+//------------------------------------------------------------------------------------
+
+    user.register = function(res, req, userInfo, database, callback){
+        var username = userInfo.username;
+        var password = userInfo.password;
+        var email = userInfo.email;
         // get the collection
         var collection = database.collection(collectionName);
         collection.findOne({_id: username}, function(err, userObj){
-            if (err) {
-                callback;
-                return res.status(500).end(err);
-            }
-            if (userObj){
-                callback
-                return res.status(409).end("username " + username + " already exists\n");
-            }
+            // error checking
+            if (err) return response(res, 500, err, callback);
+            if (userObj) return response(res, 409, "username " + username + " already exists\n", callback);
+            
             // generate a hash, and salt the hash with the password
             var salt = crypto.randomBytes(16).toString('base64');
             var saltedHash = generateHash(password, salt);
-            // add the user in
-            collection.update({_id: username},{_id: username, saltedHash: saltedHash, salt: salt}, {upsert: true}, function(err){
-                if (err) return res.status(500).end(err);
+            // add the user to database
+            collection.update({_id: username},{_id: username, saltedHash: saltedHash, salt: salt, email: email}, {upsert: true}, function(err){
+                if (err) return response(res, 500, err, callback);
                 callback;
                 return res.json("user " + username + " signed up");
             });
         });
     }
+
+
+    user.login = function(res, req, userInfo, database, callback){
+        var username = userInfo.username;
+        var password = userInfo.password;
+        // get the collection
+        var collection = database.collection(collectionName);
+        collection.findOne({_id: username}, function(err, userObj){
+            if(err) return response(res, 500, err, callback);
+            if(!userObj) return response(res, 401, "user " + username + " not found", callback);
+            if (userObj.saltedHash !== generateHash(password, userObj.salt)) return response(res, 401, ERRMSG_ACCESS_DENIED, callback);
+            req.session.username = user._id;
+            return res.redirect("/");
+        });
+    }
+
+
 
 
     return user;

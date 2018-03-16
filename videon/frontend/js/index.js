2c701c184423e6edcf1ad4f3ad56bbf3cc2fedfb
David Yau <david.yau@mail.utoronto.ca>
2018-03-16 10:43:17 -0400

    added login and register js files; need to implement api then uncomment the parts in both files

---
+++ videon/frontend/js/index.js
@@ -0,0 +1,10 @@
+/*jshint esversion: 6 */ 
+
+(function(){
+    "use strict";
+
+    window.addEventListener('load', function(){
+        // need an api method to get current user and if it's null then load login page
+        location.href = "/login.html";
+    });
+}());
